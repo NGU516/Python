76656f8edba3009ec8723effc682c595a5c904eb
NGU516 <84605433+NGU516@users.noreply.github.com>
2023-06-28 18:05:17 +0900

    Add files via upload

---
+++ 백준/11286(절댓값힙).py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+input = sys.stdin.readline\
+
+N = int(input())
+
+arr = []
+
+for _ in range(N):
+    temp = int(input())
+    if temp == 0 and arr != []: 
+        print(heapq.heappop(arr)[1])
+    elif temp == 0 and arr == []: # 비어있을 경우
+        print(0)
+    else:
+        heapq.heappush(arr, (abs(temp), temp))
